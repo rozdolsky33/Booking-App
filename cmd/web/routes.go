4113ee3f34e34d4a542978ae902b5106b710c748
volodymyrrozdolsky <volodymyr.rozdolsky@oracle.com>
2023-10-14 11:46:41 -0400

    adding handler tests

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,9 +12,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
