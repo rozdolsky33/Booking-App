957829af0da3d4ac6d00824299bbd75321e6704d
volodymyrrozdolsky <volodymyr.rozdolsky@oracle.com>
2023-10-17 10:02:33 -0400

    form test start

---
+++ internal/forms/forms_test.go
@@ -0,0 +1,43 @@
+package forms
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_Valid(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	isValid := form.Valid()
+	if !isValid {
+		t.Error("got invalid when should have been valid")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.Required("a", "b", "c")
+	if form.Valid() {
+		t.Error("form shows valid when required fields missing")
+	}
+
+	postData := url.Values{}
+	postData.Add("a", "a")
+	postData.Add("b", "a")
+	postData.Add("c", "a")
+
+	r, _ = http.NewRequest("POST", "/whatever", nil)
+
+	r.PostForm = postData
+	form = New(r.PostForm)
+	form.Required("a", "b", "c")
+	if !form.Valid() {
+		t.Error("shows does not have required fields when it does")
+	}
+
+}
